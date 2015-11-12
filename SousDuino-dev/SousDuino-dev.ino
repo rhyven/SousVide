4d3561524dfb3975195c22bf9ad74d06259acff6
Eric Light <eric@ericlight.com>
2015-11-12 23:34:23 +1300

    Moved control buttons to A0-A3 Must add more info about screen pins, too

--- SousDuino-dev/SousDuino-dev.ino
+++ SousDuino-dev/SousDuino-dev.ino
@@ -3,45 +3,92 @@
 
 int TARGET_TEMP = 60;
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-
-// This version requires a DS18B20 temperature probe, and uses the OneWriteNoResistor library available at https://github.com/bigjosh/OneWireNoResistor/
-// You'll also need the Dallas DS18 library from https://github.com/milesburton/Arduino-Temperature-Control-Library/
-
-// Your DS18B20 should be wired on digital pins 8 (+5v), 9 (data), and 10 (GND).
-// If using just a plain sensor, the flat side of the sensor should face into the center of the board.
+// Download the OneWriteNoResistor library from https://github.com/bigjosh/OneWireNoResistor/
 // More info about the NoResistor library at http://wp.josh.com/2014/06/23/no-external-pull-up-needed-for-ds18b20-temp-sensor/#more-1892
+// Also install the Dallas Temperature library from https://github.com/milesburton/Arduino-Temperature-Control-Library/
+
+/* Pinout:
+ *  
+ *  GND
+ *  D2 - LED screen RS
+ *  D3 - LED screen E
+ *  D4 - LED screen D4
+ *  D5 - LED screen D5
+ *  D6 - LED screen D6
+ *  D7 - LED screen D7
+ *  D8 - Temperature probe data pin  (DS18B20)
+ *  D9
+ *  D10
+ *  D11 - Relay control pin
+ *  D12
+ *  D13
+ *  3.3v
+ *  A0 - 'Up' button
+ *  A1 - 'Down' button
+ *  A2 - 'Start' button LED+  (note can't be A6 due to weird A6/A7)
+ *  A3 - 'Start' button left -- ALSO 10k resistor from A7 to GND
+ *  A4
+ *  A5
+ *  A6
+ *  A7
+ *  5v - 'Start' button right
+ *  GND - 'Start' button LED-
+ *  
+ *  
+ *  Screen PINS
+ *  A - 5v
+ *  K - GND
+ *  
+ */ 
+
 
 // Thermocouple (DS18B20) PIN labels
-int ds_data = 8;
+const int ds_data = 8;
 
 // Relay (SSR) PIN labels
-int relay = 11;
+const int relay = 11;
 
+// Button labels
+const int button_up = A0;
+const int button_down = A1;
+const int button_start_LED = A2;
+const int button_start = A3;
+const int analogHigh = 900;
+const int button_delay = 100;
+
+// Set up thermocouple libraries
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #define ONE_WIRE_BUS ds_data
 #define TEMPERATURE_PRECISION 12
-
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress thermocouple;
 
 
+// Set up LCD screen libraries
 #include <LiquidCrystal.h>
-
 // initialize the library with the numbers of the interface pins
-// RS, E, D4, D5, D6, D7
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+const int RS = 2; const int E = 3; const int D4 = 4; const int D5 = 5; const int D6 = 6; const int D7 = 7;
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 
 
+// Set up other variables
 bool HEATING = false;
+bool start_button_pressed = false;
+unsigned long lastPress = 0;
 
 void setup(void)
 {
   Serial.begin(9600);
 
+  Serial.println("Initialising variables");
 
-
+  pinMode(button_up, INPUT);
+  pinMode(button_down, INPUT);
+  pinMode(button_start, INPUT);
+  pinMode(button_start_LED, OUTPUT);
+  
   // Initialse Relay pins
   pinMode(relay, OUTPUT);  digitalWrite(relay, LOW);
 
@@ -51,8 +98,44 @@ void setup(void)
 }
 
 
+void check_buttons() {
+
+  // read the state of the up button
+
+  // read the state of the down button
+
+  // Shit I'm sure there's a better way to do this.  Gonna have to break this out into a function.
+
+  // read the state of the start button
+  int button_start_voltage = analogRead(button_start);
+  Serial.println("Start button voltage = " + String(button_start_voltage));
+
+  // Check to see if the start button is being pressed
+  if (button_start_voltage > analogHigh) {
+    
+    // Only respond if it's been a delay since last press
+    if (millis() - lastPress > button_delay) {
+      start_button_pressed = !start_button_pressed; 
+    }
+    
+    lastPress = millis();
+  }
+
+  // Update Start button LED state
+  digitalWrite(button_start_LED, start_button_pressed);
+  
+}
+
 void loop(void)
 {
+
+/*
+ * I really want to be checking the state of all the buttons every loop
+ * Need to check button_up, button_down, and button_start
+ * 
+ */
+  
+  
   // Read temperature
   sensors.getAddress(thermocouple, 0);
   sensors.begin();
@@ -60,11 +143,21 @@ void loop(void)
   float tempC = sensors.getTempC(thermocouple);  // Convert temperature into Celcius
 
 
+  check_buttons();
+
+
+
+
+
+
+
+
   // Make sure we're getting usable readings
   if (tempC < 0) {
     Serial.print("Unplugged");
     relay_off();
     led_unplugged();
+    delay(500);
   }
 
   // See if we need to heat
@@ -116,7 +209,6 @@ void led_unplugged() {
   lcd.print("Probe unplugged!");
   lcd.setCursor(0, 1);
   lcd.print("Waiting for data");
-
 }
 
 void led_update(float target, float current, bool Heating)
